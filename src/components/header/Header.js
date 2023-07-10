7dcaddd2713dc8522f93ee44a9bc99e8a6fe0321
Rajesh Dasari <venkatarajesh.dasari@gmail.com>
2023-07-10 19:33:38 +0530

    rajesh changes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
   constructor(props) {
     super(props);
     this.userData = {
-      name: "Veerendra Kumar",
+      name: "Dasari Venkata Rajesh",
       age: "29",
     };
 
--- src/components/main-content/MainContent.js
+++ src/components/main-content/MainContent.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 const MainContent = (props) => {
   let headerName = "Test";
   const [titleName, setTitleName] = useState("Main section");
+  const [count, setcount] = useState(0);
 
   const handleClick = () => {
     setTitleName("Clicked Section");
@@ -12,7 +13,7 @@ const MainContent = (props) => {
 
   return (
     <div>
-      <div style={{ color: "red", padding: "10px", marginLeft: "30px" }}>
+      <div style={{ color: "cadetblue", padding: "10px", marginLeft: "30px" }}>
         <h2 id="demo">
           This is {titleName} {headerName} is coming from the user{" "}
           {props.userData.name} and the age is {props.userData.age}
@@ -24,6 +25,15 @@ const MainContent = (props) => {
         for context, This is content section used for context, This is content
         section used for context
       </p>
+      <br />
+      <br />
+      <button onClick={() => setcount(count + 1)}>Increment</button> &nbsp;
+      {count} &nbsp;
+      <button onClick={() => setcount(count - 1)}>Decrement</button> &nbsp;
+      <button onClick={() => setcount(0)}>Reset</button>
+      <br />
+      <br />
+      <br />
       <button onClick={handleClick}>Change Name</button>
       {/* <button onclick="handleClick()">Change Name</button> */}
       <br />
