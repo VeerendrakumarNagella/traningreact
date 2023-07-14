fc9776dc2b784529607efacea1a532ff15ef6de0
SurekhaGithub <surekhak2589@gmail.com>
2023-07-13 22:42:21 -0400

    users data added

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,11 +1,46 @@
 import React from "react"
+import './Users.css'
 import {useState,useEffect} from "react"
 
 
+
 const Users = () => {
+    const [users,setUsers]=useState([])
+    const getData=async()=>{
+        const data=await fetch("https://reqres.in/api/users");
+        const json=await data.json();
+        setUsers(json.data);
+        console.log(users);
+    }
+    useEffect(()=>{
+        getData()
+
+    },[])
+
   return (
     <div>
-      <h1>UsersData</h1>
+      <h1 style={{color:"red",marginTop:"15px"}}>UsersData</h1>
+      <table className="table">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Avatar</th>
+            </tr>
+        </thead>
+        <tbody>
+            {users && users.map(user=>
+                <tr key={user.id}>
+                <td>{user.id}</td> 
+                <td>{user.first_name}{user.last_name}</td>
+                <td>{user.email}</td>
+                <td><img src={user.avatar}/></td>
+                </tr>
+                )}
+        </tbody>
+
+      </table>
     </div>
   )
 }
