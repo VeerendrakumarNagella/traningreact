fd1ecd3014892ee4078d382f37f3af06cfb53320
Kiran Malepati <kiran.malepati235@gmail.com>
2023-07-18 07:22:17 +0530

    colour-change

--- src/components/main-content/MainContent.js
+++ src/components/main-content/MainContent.js
@@ -12,7 +12,7 @@ const MainContent = (props) => {
 
   return (
     <div>
-      <div style={{ color: "pink", padding: "10px", marginLeft: "30px" }}>
+      <div style={{ color: "green", padding: "10px", marginLeft: "30px" }}>
         <h2 id="demo">
           This is {titleName} {headerName} is coming from the user{" "}
           {props.userData.name} and the age is {props.userData.age}
