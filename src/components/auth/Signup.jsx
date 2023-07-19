62b966d6d61c565c1bf426b6dc38ffdf19be004d
veerendra <veerendra.nagella@gmail.com>
2023-07-19 11:31:57 +0530

    validations

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 /*
 const field1 = "userName";
@@ -29,12 +29,60 @@ const Signup = () => {
     gender: "",
     lang: [],
   });
+  const [userError, setuserError] = useState({
+    fullNameError: "",
+    emailError: "",
+    phoneNumberError: "",
+    passwordError: "",
+    ageError: " ",
+    dobError: "",
+    bioError: "",
+    photoError: "",
+    cityError: " ",
+    genderError: "",
+    langError: "",
+  });
   const { fullName, email, phoneNumber, password, age, dob, bio, city, lang } =
     user;
+  const {
+    fullNameError,
+    emailError,
+    phoneNumberError,
+    passwordError,
+    ageError,
+    dobError,
+    photoError,
+    bioError,
+    cityError,
+    genderError,
+    langError,
+  } = userError;
+
+  const fullNameRef = useRef();
+
+  const getValidationErrorFocus = (
+    ref,
+    errorProp,
+    defaultErrorMessage = "This is field is mandatory"
+  ) => {
+    setuserError({
+      ...userError,
+      [errorProp]: errorProp || defaultErrorMessage,
+    });
+    ref.current.focus();
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(user);
+    if (!fullName || fullNameError) {
+      getValidationErrorFocus(
+        fullNameRef,
+        fullNameError,
+        "Please enter your fullname"
+      );
+    } else {
+      console.log(user);
+    }
   };
 
   const handleCancel = (e) => {
@@ -61,6 +109,30 @@ const Signup = () => {
         [name]: value,
       });
     }
+
+    if (name === "fullName") {
+      if (!value) {
+        setuserError({
+          ...userError,
+          fullNameError: "Please enter your fullname",
+        });
+      } else if (!/^[A-Za-z\s]*$/.test(value)) {
+        setuserError({
+          ...userError,
+          fullNameError: "Name should be having only charecters and space",
+        });
+      } else if (value.length <= 8 || value.length > 20) {
+        setuserError({
+          ...userError,
+          fullNameError: "Name should in between 8 to 20 bcharecteres only",
+        });
+      } else {
+        setuserError({
+          ...userError,
+          fullNameError: "",
+        });
+      }
+    }
   };
 
   return (
@@ -79,7 +151,9 @@ const Signup = () => {
             name="fullName"
             value={fullName}
             onChange={handleChange}
+            ref={fullNameRef}
           />
+          {fullNameError && <p style={{ color: "red" }}>{fullNameError}</p>}
         </div>
         <div className="form-item">
           <label htmlFor="email" className="form-item-label">
@@ -94,6 +168,7 @@ const Signup = () => {
             value={email}
             onChange={handleChange}
           />
+          {emailError && <p style={{ color: "red" }}>{emailError}</p>}
         </div>
         <div className="form-item">
           <label htmlFor="password" className="form-item-label">
@@ -108,6 +183,7 @@ const Signup = () => {
             value={password}
             onChange={handleChange}
           />
+          {passwordError && <p style={{ color: "red" }}>{passwordError}</p>}
         </div>
         <div className="form-item">
           <label htmlFor="phoneNumber" className="form-item-label">
@@ -122,6 +198,9 @@ const Signup = () => {
             value={phoneNumber}
             onChange={handleChange}
           />
+          {phoneNumberError && (
+            <p style={{ color: "red" }}>{phoneNumberError}</p>
+          )}
         </div>
         <div className="form-item">
           <label htmlFor="age" className="form-item-label">
@@ -136,6 +215,7 @@ const Signup = () => {
             value={age}
             onChange={handleChange}
           />
+          {ageError && <p style={{ color: "red" }}>{ageError}</p>}
         </div>
         <div className="form-item">
           <label htmlFor="dob" className="form-item-label">
@@ -150,6 +230,7 @@ const Signup = () => {
             value={dob}
             onChange={handleChange}
           />
+          {dobError && <p style={{ color: "red" }}>{dobError}</p>}
         </div>
         <div className="form-item">
           <label htmlFor="photo" className="form-item-label">
@@ -162,6 +243,7 @@ const Signup = () => {
             name="photo"
             onChange={handleChange}
           />
+          {photoError && <p style={{ color: "red" }}>{photoError}</p>}
         </div>
         <div className="form-item">
           <label htmlFor="city" className="form-item-label">
@@ -179,6 +261,7 @@ const Signup = () => {
             <option value="bng">Banglore</option>
             <option value="goa">Goa</option>
           </select>
+          {cityError && <p style={{ color: "red" }}>{cityError}</p>}
         </div>
         <div className="form-item radio">
           <label htmlFor="female" className="form-item-label">
@@ -206,6 +289,7 @@ const Signup = () => {
               />
             </label>
           </div>
+          {genderError && <p style={{ color: "red" }}>{genderError}</p>}
         </div>
         <div className="form-item radio">
           <label htmlFor="telugu" className="form-item-label">
@@ -233,6 +317,7 @@ const Signup = () => {
               />
             </label>
           </div>
+          {langError && <p style={{ color: "red" }}>{langError}</p>}
         </div>
         <div className="form-item">
           <label htmlFor="bio" className="form-item-label">
@@ -246,6 +331,7 @@ const Signup = () => {
             value={bio}
             onChange={handleChange}
           />
+          {bioError && <p style={{ color: "red" }}>{bioError}</p>}
         </div>
         <div className="form-item btn-group">
           <button className="btn alert" onClick={handleCancel}>
