5751d778f6afdf7cce6cd3bd16493dab8e916139
SurekhaGithub <surekhak2589@gmail.com>
2023-07-24 12:46:51 -0400

    user changes

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,51 +1,51 @@
-import React from "react"
-import './Users.css'
-import {useState,useEffect} from "react"
-
-
+import React from "react";
+import "./Users.css";
+import { useState, useEffect } from "react";
 
 const Users = () => {
-    const [users,setUsers]=useState([]);
-    
+  const [users, setUsers] = useState([]);
 
-    const getData=async()=>{
-        const data=await fetch("https://reqres.in/api/users");
-        const json=await data.json();
-        setUsers(json.data);
-        console.log(users);
-    }
-    useEffect(()=>{
-        getData()
-
-    },[])
+  const getData = async () => {
+    const data = await fetch("https://reqres.in/api/users");
+    const json = await data.json();
+    setUsers(json.data);
+    console.log(users);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <div>
-    <h2>Users Data</h2>
+      <h2>Users Data</h2>
       <table className="table">
         <thead>
-            <tr>
-                <th>Id</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Avatar</th>
-            </tr>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Avatar</th>
+          </tr>
         </thead>
         <tbody>
-            {users && users.map(user=>
-                <tr key={user.id}>
-                <td>{user.id}</td> 
-                <td>{user.first_name}{user.last_name}</td>
+          {users &&
+            users.map((user) => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>
+                  {user.first_name}
+                  {user.last_name}
+                </td>
                 <td>{user.email}</td>
-                <td><img src={user.avatar}/></td>
-                </tr>
-                )}
+                <td>
+                  <img src={user.avatar} />
+                </td>
+              </tr>
+            ))}
         </tbody>
-
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Users
-
+export default Users;
